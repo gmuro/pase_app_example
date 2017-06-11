fdbfd17bde520c91d57fe4565085250200df5f73
Gustavo Muro <gustmuro@gmail.com>
2017-06-11 11:04:08 -0300

    add application 2

---
+++ application2/src/pase_app_example.c
@@ -0,0 +1,160 @@
+/* Copyright 2017, Gustavo Muro
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \brief PASE APP EXAMPLE
+ **
+ ** ejemplo de aplicación usando CIAA Firmware
+ **
+ **/
+
+/** \addtogroup
+ ** @{ */
+/** \addtogroup
+ ** @{ */
+/** \addtogroup
+ ** @{ */
+
+/*==================[inclusions]=============================================*/
+#include "os.h"
+#include "pase_app_example.h"
+#include "bsp.h"
+#include "stdint.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+static uint32_t timerUp10ms = 0;
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+/** \brief Main function
+ *
+ * This is the main entry point of the software.
+ *
+ * \returns 0
+ *
+ * \remarks This function never returns. Return value is only to avoid compiler
+ *          warnings or errors.
+ */
+int main(void)
+{
+   /* Starts the operating system in the Application Mode 1 */
+   /* This example has only one Application Mode */
+   StartOS(AppMode1);
+
+   /* StartOs shall never returns, but to avoid compiler warnings or errors
+    * 0 is returned */
+   return 0;
+}
+
+/** \brief Error Hook function
+ *
+ * This fucntion is called from the os if an os interface (API) returns an
+ * error. Is for debugging proposes. If called this function triggers a
+ * ShutdownOs which ends in a while(1).
+ *
+ * The values:
+ *    OSErrorGetServiceId
+ *    OSErrorGetParam1
+ *    OSErrorGetParam2
+ *    OSErrorGetParam3
+ *    OSErrorGetRet
+ *
+ * will provide you the interface, the input parameters and the returned value.
+ * For more details see the OSEK specification:
+ * http://portal.osek-vdx.org/files/pdf/specs/os223.pdf
+ *
+ */
+void ErrorHook(void)
+{
+   ShutdownOS(0);
+}
+
+/** \brief Initial task
+ *
+ * This task is started automatically in the application mode 1.
+ */
+TASK(InitTask)
+{
+   bsp_init();
+
+   ActivateTask(LoopTask1);
+   ActivateTask(LoopTask2);
+
+   TerminateTask();
+}
+
+TASK(LoopTask1)
+{
+   int32_t tempCont;
+
+   while (1)
+   {
+	  if (timerUp10ms & 0b10000)
+		  bsp_ledAction(BOARD_LED_ID_RED, BSP_LED_ACTION_ON);
+	  else
+		  bsp_ledAction(BOARD_LED_ID_RED, BSP_LED_ACTION_OFF);
+   }
+}
+
+TASK(LoopTask2)
+{
+   int32_t tempCont;
+
+   while (1)
+   {
+     if (timerUp10ms & 0b10000)
+        bsp_ledAction(BOARD_LED_ID_YELLOW, BSP_LED_ACTION_ON);
+     else
+        bsp_ledAction(BOARD_LED_ID_YELLOW, BSP_LED_ACTION_OFF);
+   }
+}
+
+ALARMCALLBACK(CallBack10ms)
+{
+	timerUp10ms++;
+}
+
+
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+
