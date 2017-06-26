30f29d2b23bc9645a372c98ab160e4493d8d7cfe
Joaquin Rodriguez <joaquinrodriguez1991@gmail.com>
2017-06-26 09:52:06 -0300

    Prototypes on h file for uart created

--- mcu/cortexM4/lpc43xx/lpc4337/inc/mcu_uart.h
+++ mcu/cortexM4/lpc43xx/lpc4337/inc/mcu_uart.h
@@ -1,4 +1,15 @@
 #ifndef __MCU_UART_H__
 #define __MCU_UART_H__
+#include <stdint.h>
+#include <stdio.h>
+
+void mcu_uart_init(int32_t baudRate);
+void mcu_uart_enable(void);
+void mcu_uart_disable(void);
+void mcu_uart_setBaud(int32_t baudRate);
+void mcu_uart_setFifoTriggerLevel(int32_t level);
+void mcu_uart_enableTXInterrupt(bool isEnable);
+int32_t mcu_uart_read(uint8_t* buffer, size_t const size);
+int32_t mcu_uart_write(uint8_t const * const buffer, size_t const size);
 
 #endif
